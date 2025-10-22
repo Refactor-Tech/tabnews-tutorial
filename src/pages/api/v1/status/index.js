6013324512ffe12aaf18dcf09e83945f5e859661
Matheus Pavanelli <mspavanelli@gmail.com>
2025-10-22 00:24:29 -0300

    feat: add PostgreSQL configuration and database query functionality

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(_, response) {
+import database from "../../../../../infra/database.js";
+
+export default async function status(_, response) {
+  const result = await database.query("SELECT 1 + 2 as sum;");
+  console.log(result.rows);
   return response.status(200).json({ message: "API is working" });
 }
